7b935ab6c38eabdede444e2655260a6f45fe7a9f
Fr1son <ihor.malenkov18@gmail.com>
2023-10-08 17:41:48 +0300

    second hm

---
+++ hm2/index.js
@@ -0,0 +1,13 @@
+let num1 = prompt('Введіть перше число');
+let num2 = prompt('Введіть друге число');
+
+let Sum = Number(num1) + Number(num2);
+let Diff = num1 - num2;
+let Mult = num1 * num2;
+let Div = num1 / num2;
+
+console.log('Calculations є finished!');
+console.log('Sum: ' + num1 + ' + ' + num2 + ' = ' + Sum);
+console.log('Diff: ' + num1 + ' - ' + num2 + ' = ' + Diff);
+console.log('Mult: ' + num1 + ' * ' + num2 + ' = ' + Mult);
+console.log('Div: ' + num1 + ' / ' + num2 + ' = ' + Div);
