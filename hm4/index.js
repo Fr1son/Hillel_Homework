d700a6204dbe35de8a8029635f131745d489b4c2
Fr1son <ihor.malenkov18@gmail.com>
2023-10-11 19:30:12 +0300

    task1 done

---
+++ hm4/index.js
@@ -0,0 +1,8 @@
+let num1 = Number(prompt("Введіть перше число"));
+let num2 = Number(prompt("Введіть друге число"));
+
+if (num1 > num2){
+  console.log(num1 + " більше ніж " + num2)
+}else{
+  console.log(num1 + " менше ніж " + num2)
+};
