4e074b785a551abd1f5f3ddc7d412aa4c38ca944
Fr1son <ihor.malenkov18@gmail.com>
2023-10-11 22:30:34 +0300

    task5 done

---
+++ hm4/index5.js
@@ -0,0 +1,9 @@
+let num = prompt("Введіть двохзначне число");
+if (num[0] > num[1]){
+  console.log(num[0] + " більше ніж " + num[1])
+}else if(num[0] < num[1]){
+  console.log(num[0] + " меньше ніж " + num[1])
+}else{
+  console.log("числа рівні")
+};
+
